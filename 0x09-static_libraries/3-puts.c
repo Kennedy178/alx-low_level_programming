90e5dfea775e41a6cc8aa6f542215a214211e8cf
Kennedy178 <kennedykm178@gmail.com>
2023-03-10 03:09:01 -0800

    all .c files

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+void _puts(char *str)
+
+/* _puts -  Entry point
+*
+ * Return: Always 0 (Success)
+ */
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

8612e87e92ab053124b155c39b21f1f2acb3f56b
Kennedy178 <kennedykm178@gmail.com>
2023-03-15 05:40:21 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,49 @@
+#include "main.h"
 #include <stdlib.h>
-#include "holberton.h"
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
- *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * str_concat - function declaration
+ * @s1: string 1
+ * @s2: string 2
+ * Return: as string value
  */
+
+char *str_concat(char *s1, char *s2);
+
+/**
+ * str_concat - function definition
+ * @s1: the first string
+ * @s2: the second string
+ * Description: concatenates s1 and s2
+ * Return: the concatenated string
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *new_string;
+	int index;
+	int concat_index = 0;
+	int len = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+		s1 = "";
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	new_string  = malloc(sizeof(char) * len);
+
+	if (new_string == NULL)
 		return (NULL);
 
-	i = 0;
-	j = 0;
+	for (index = 0; s1[index]; index++)
+		new_string[concat_index++] = s1[index];
 
-	if (s1)
-	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
-	}
+	for (index = 0; s2[index]; index++)
+		new_string[concat_index++] = s2[index];
 
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	s3[i] = '\0';
-
-	return (s3);
+	return (new_string);
 }

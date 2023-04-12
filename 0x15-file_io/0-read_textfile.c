7e35d11cdf9b420737297d3335821dd51c82edf6
Kennedy178 <kennedykm178@gmail.com>
2023-04-12 12:06:10 -0700

    eleventh time

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
  * Return: 0 if fails or filename is NULL.
  */
 
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 
 char *fer;
@@ -20,7 +20,7 @@ if (ken == -1)
 return (0);
 fer = malloc(sizeof(char) * letters);
 kenne = read(ken, fer, letters);
-kene = write(STDOUT_FILEN0, fer, kenne);
+kene = write(STDOUT_FILENO, fer, kenne);
 free(fer);
 close(ken);
 return (kene);
